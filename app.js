84574aa1e629ed5b5fdac2189f2f3de647687185
Frederic NIETO <fnieto@e2r4p9.42.fr>
2016-01-23 12:34:03 +0100

    trying to fix angular

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module('AddressBook', []);
 
-app.controller('LiCtrl', function($scope) {
+app.controller('LiCtrl', function($scope, $http) {
     
     $http.get('http://cdn.bemyapp.com/files/2016/one-point/fixtures/challenge-full-stack/addressBook.json')
        .then(function(res){
