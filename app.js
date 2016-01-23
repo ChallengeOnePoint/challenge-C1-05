8397d2082b01390a09172e6ecc669f1eca301da3
Frederic NIETO <fnieto@e2r4p9.42.fr>
2016-01-23 12:22:34 +0100

    included services.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var app = angular.module('AddressBook', []);
+
+app.controller('LiCtrl', function($scope) {
+    
+    $http.get('http://cdn.bemyapp.com/files/2016/one-point/fixtures/challenge-full-stack/addressBook.json')
+       .then(function(res){
+          $scope.addresses = res.data;
+        });
+    
+});
